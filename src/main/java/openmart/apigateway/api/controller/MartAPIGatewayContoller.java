a89b205df295538243a6f9e21ea7dcf5184276ff
KimSungJun <arduous1130@gmail.com>
2020-03-31 22:48:24 +0900

    [#20200331] initial commit

---
+++ src/main/java/openmart/apigateway/api/controller/MartAPIGatewayContoller.java
@@ -0,0 +1,19 @@
+//
+//package openmart.apigateway.api.controller;
+//
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import javax.servlet.http.HttpServletRequest;
+//
+///**
+// * The type Mart api contoller.
+// */
+//@RestController
+//@RequestMapping(path = "/v1/mart", produces = "application/json")
+//public class MartAPIGatewayContoller {
+//	@RequestMapping("/")
+//	String greet(HttpServletRequest req){
+//		return "<H1>Search Gateway Powered By Zuul</H1>";
+//	}
+//}
